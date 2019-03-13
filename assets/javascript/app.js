649c7a53ac45c93b005df6c927aa4cdb89f106cf
David Toth <dtoth2016@gmail.com>
2019-03-13 13:38:56 -0500

    Preliminary code for backend

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,35 @@
-$("button").on("click", function(){
+var APIKey = "xBQI8f2h0EzwECGopaYlE7ywDT3s61UZ";
+
+$(document).ready(function() {
+
+    $("button").on("click", function(event) {
+        event.preventDefault();
+        var numArticle = $("#num-records").val();
+
+        while (numArticle > 0) {
+            
+        }
+    });
+
+    function apiCall(pageNumber){
+
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startDate + "0101&end_date=" + endDate + 
+                    "1231&q=" + "something&api-key=" + APIKey;
+
+        $.ajax({
+            url = queryURL,
+            method: 'GET'
+        })
+        .then(function(response){
 
 
-$.ajax({
-    url = queryURL,
-    method: 'GET'
-})
-.then(function(response){
 
-})
-}
-);
+        })
+
+    }
+
+    function displayArticles( /* Takes in Array of articles */) {
+        // display articles
+    }
+
+});
