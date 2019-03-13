73619436d52b583bcb40330f9ace4ea25013f79d
David Toth <dtoth2016@gmail.com>
2019-03-13 13:52:27 -0500

    code for page number

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,19 @@ $(document).ready(function() {
 
     $("button").on("click", function(event) {
         event.preventDefault();
-        var numArticle = $("#num-records").val();
+        var numArticles = $("#num-records").val();
 
-        while (numArticle > 0) {
-            
+        for (var i = 0; i <= Math.floor((numArticles - 1) / 10); i++) {
+
+            if (i === Math.floor( (numArticles - 1) /10) ){
+                apiCall(i, numArticles % 10);
+            } else {
+                apiCall(i, 10);
+            }
         }
     });
 
-    function apiCall(pageNumber){
+    function apiCall(pageNumber, numArticles){
 
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startDate + "0101&end_date=" + endDate + 
                     "1231&q=" + "something&api-key=" + APIKey;
